b88dbc852c00d6fdc2a674fc708920519376dae1
Diana Alves <dianaalves239@hotmail.com>
2024-06-05 15:58:59 -0300

    criado desafio ContaTerminal

---
+++ meu_primeiro_projeto_java/src/edu/diana/ContaBanco/ContaTerminal.java
@@ -0,0 +1,23 @@
+package edu.diana.ContaBanco;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite a sua conta");
+        int numero = scanner.nextInt();
+        System.out.println("Digite a sua Agência");
+        String agencia = scanner.next();
+        System.out.println("Digite o seu nome");
+        String nome = scanner.next();
+        System.out.println("Digite o seu saldo");
+        double saldo= scanner.nextDouble();
+
+        System.out.println("Ola," + nome + " obrigada por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e o seu saldo R$ " + saldo + " já está disponivel para saque.");
+        System.out.println("Obrigada por confiar em nos! ");
+    }
+}
